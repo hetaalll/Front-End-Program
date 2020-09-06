4951f8591fd553857c135d6afa144d34d4f1acdb
hetalp1698 <hetalp1698@gmail.com>
2020-09-06 09:33:15 -0400

    intial commit

--- Evaluate News NLP/src/server/index.js
+++ Evaluate News NLP/src/server/index.js
@@ -2,6 +2,15 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+const dotenv = require('dotenv');
+dotenv.config();
+// console.log(`Your API key is ${process.env.API_KEY}`);
+
+var textapi = {
+    application_key: process.env.API_KEY
+};
+
+
 const app = express()
 
 app.use(express.static('dist'))
@@ -14,9 +23,9 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+    })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
